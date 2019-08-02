762a1853ba63996a8fd43ffeba858cf9e258ce93
Davi <davi.guimaraesleite@gmail.com>
2019-08-02 20:05:49 -0300

    setting up rack attack.

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -4,10 +4,10 @@ class Rack::Attack
   throttle('req/ip', limit: 300, period: 5.minutes, &:ip)
 
   throttle('logins/email', limit: 5, period: 20.seconds) do |req|
-    req.params['email'].presence if req.path == '/authenticate' && req.post?
+    req.params['email'].presence if req.path.eql?('/authenticate') && req.post?
   end
 
   throttle('logins/ip', limit: 5, period: 20.seconds) do |req|
-    req.ip if req.path == '/authenticate' && req.post?
+    req.ip if req.path.eql?('/authenticate') && req.post?
   end
 end
