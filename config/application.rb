469e9d2e8bef8a5815a8ba05d276ecc270908bf8
Davi <davi.guimaraesleite@gmail.com>
2019-08-02 20:04:39 -0300

    setting up rack attack.

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module ApiApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use Rack::Attack
   end
 end
---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Rack::Attack
+  throttle('req/ip', limit: 300, period: 5.minutes, &:ip)
+
+  throttle('logins/email', limit: 5, period: 20.seconds) do |req|
+    req.params['email'].presence if req.path == '/authenticate' && req.post?
+  end
+
+  throttle('logins/ip', limit: 5, period: 20.seconds) do |req|
+    req.ip if req.path == '/authenticate' && req.post?
+  end
+end
