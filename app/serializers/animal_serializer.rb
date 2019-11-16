b60f87b121537e1ab37a356da70913e0886e0461
Davi <davi.guimaraesleite@gmail.com>
2019-11-16 16:53:53 -0300

    modifying create_adopter method.

--- app/serializers/animal_serializer.rb
+++ app/serializers/animal_serializer.rb
@@ -1,14 +1,14 @@
 class AnimalSerializer < ActiveModel::Serializer
-	include ActionView::Helpers::DateHelper
+  include ActionView::Helpers::DateHelper
 
   attributes :id, :name, :description, :age, :adopted, :specie, 
   					 :size, :avatar, :castrated, :gender, :weight, :created
 
   def created
-  	distance_of_time_in_words(object.created_at, Time.now)
+    distance_of_time_in_words(object.created_at, Time.now)
   end
 
   def id
-  	object.id.to_s
+    object.id.to_s
   end
 end
