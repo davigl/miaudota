9b97a52db1c188728daff572b313ca29b71c9f80
Davi <davi.guimaraesleite@gmail.com>
2019-12-08 18:02:59 -0300

    fix empty model serializer.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::API
     if model.any?
       render json: model, custom_element: custom_element, status: status
     else
-      render json: model
+      render json: { data: [] }, status: :not_found
     end
   end
 
